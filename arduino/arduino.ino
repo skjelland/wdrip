dec331f3b0aea65240a82bbcead88acd7bc39823
Morten Skjelland <Morten.Skjelland@imatis.com>
2016-03-16 23:26:37 +0100

    remove unused includes

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,8 +1,5 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 #include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <WiFiUdp.h>
 #include <SoftwareSerial.h>
 
 #define AP_SSID "zombie"
